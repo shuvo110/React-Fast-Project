a1952278925a22fd6994ddbbcc7c5b3339fe066e
Shuvo <sr9891710@gmail.com>
2025-04-25 11:45:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Root from './components/Root'
+function App() {
+  return (
+    <div>
+       <Root />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navber from './Navber'
+
+function Root() {
+  return (
+    <div>
+        <Navber></Navber>
+    </div>
+  )
+}
+
+export default Root
